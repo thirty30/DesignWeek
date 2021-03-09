cace614411c83ac3ce43fe962e70f019d8687a7b
Adrian Cogswell <cogs.adrian@gmail.com>
2021-03-09 18:10:48 -0500

    Grid movement created
    test scene for tuesday prototype created

---
+++ MagicalPlace/Assets/Scripts/GridMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public Vector2 moveToPoint;
+    public bool isMoving;
+
+
+    void Update()
+    {
+        if (isMoving)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, moveToPoint, moveSpeed * Time.deltaTime);
+
+            if (Vector2.Distance(transform.position, moveToPoint)<= 0.5f)
+            {
+                transform.position = moveToPoint;
+                isMoving = false;
+                moveToPoint = new Vector2(0, 0);
+            }
+        }
+
+        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f && !isMoving)
+        {
+            SetNewMoveLocation();
+        }
+        if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f && !isMoving)
+        {
+            SetNewMoveLocation();
+        }
+    }
+
+    void SetNewMoveLocation()
+    {
+        moveToPoint = new Vector2(transform.position.x + Input.GetAxisRaw("Horizontal"), transform.position.y + Input.GetAxisRaw("Vertical"));
+        isMoving = true;
+    }
+}
