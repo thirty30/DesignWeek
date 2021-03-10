48b3b506195654c69877b093d0d104937a0ef6d6
Adrian Cogswell <cogs.adrian@gmail.com>
2021-03-09 19:43:46 -0500

    Create soup prefab when pressing space
    soup destroys itself after 1 sec

---
+++ MagicalPlace/Assets/Scripts/SpillSoup.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpillSoup : MonoBehaviour
+{
+    public GameObject soup;
+    public float timerMax;
+
+    private bool isRecharging = false;
+    private float timer;
+    private float timerSeconds;
+    
+    void Update()
+    {
+        if (Mathf.Abs(Input.GetAxisRaw("Jump")) == 1 && !isRecharging)
+        {
+            CreateSoup();
+        }
+
+        if (isRecharging)
+        {
+            timer++;
+            if (timer % 60 == 0)
+            {
+                timerSeconds++;
+            }
+
+            if (timerSeconds >= timerMax)
+            {
+                isRecharging = false;
+                timerSeconds = 0;
+            }
+        }
+    }
+
+    void CreateSoup()
+    {
+        Instantiate(soup, new Vector3(transform.position.x, transform.position.y), Quaternion.identity);
+        isRecharging = true;
+    }
+}
