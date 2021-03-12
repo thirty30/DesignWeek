5353a6f35ef55de919ea64af81b7424a4a192a6f
Adrian Cogswell <cogs.adrian@gmail.com>
2021-03-11 19:52:54 -0500

    Implemented background to level and win/lose scenes

--- MagicalPlace/Assets/MoveToGameScene.cs
+++ MagicalPlace/Assets/MoveToGameScene.cs
@@ -5,6 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MoveToGameScene : MonoBehaviour
 {
+    private void FixedUpdate()
+    {
+        if (Mathf.Abs(Input.GetAxisRaw("Jump")) == 1)
+        {
+            ChangeScene();
+        }
+    }
     public void ChangeScene()
     {
         SceneManager.LoadScene("InteractablesTest");
